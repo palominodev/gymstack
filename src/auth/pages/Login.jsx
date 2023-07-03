056e5ec52ae7ffecb3cfede9026c4915012b1418
palominodev <jeremypalomino.dev@gmail.com>
2023-07-02 20:33:47 -0500

    Hook y Login

--- src/auth/pages/Login.jsx
+++ src/auth/pages/Login.jsx
@@ -1,5 +1,29 @@
+import { useForm } from "../../hooks/useForm"
+
 export const Login = () => {
+  const {correo, password, onInputChange} = useForm({
+    correo: "",
+    password: "",
+  })
   return (
-	<div>Login</div>
+    <div>
+      <form>
+        <label htmlFor="correo">Correo</label>
+        <input 
+          value={correo}
+          onChange={onInputChange}
+          type="email" 
+          name="correo" 
+          id="correo" />
+        <br />
+        <label htmlFor="password">Contraseña</label>
+        <input
+          value={password}
+          onChange={onInputChange} 
+          type="password" 
+          name="password" 
+          id="password" />
+      </form>
+    </div>
   )
 }
