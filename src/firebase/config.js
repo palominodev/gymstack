9534232a71eaf640dab979706f0711dbdbde9fff
palominodev <jeremypalomino.dev@gmail.com>
2023-07-02 20:10:33 -0500

    Firebase Config

---
+++ src/firebase/config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app"
+import { getAuth } from "firebase/auth"
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDE33C0d_zZhlJtmEtrO1qWBHOxaBgunTg",
+  authDomain: "gymstack-caeaf.firebaseapp.com",
+  projectId: "gymstack-caeaf",
+  storageBucket: "gymstack-caeaf.appspot.com",
+  messagingSenderId: "864620271061",
+  appId: "1:864620271061:web:82e9f8ece85f5e838c595c"
+};
+
+export const FirebaseApp = initializeApp(firebaseConfig)
+export const FirebaseAuth = getAuth(FirebaseApp)
