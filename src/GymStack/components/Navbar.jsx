7877b87dfcf60042e201851a34cfbf9b11df2f53
palominodev <jeremypalomino.dev@gmail.com>
2023-07-05 18:52:25 -0500

    Style NavBar.jsx

--- src/GymStack/components/Navbar.jsx
+++ src/GymStack/components/Navbar.jsx
@@ -2,18 +2,23 @@ import { NavLink } from "react-router-dom"
 
 export const Navbar = () => {
   return (
-	<nav>
-		<ul>
-			<li>
-				<NavLink className={'font-bold'} to={'admin'}>Inicio</NavLink>
+	<nav className="mb-6 bg-orange-500">
+		<ul className="flex justify-center">
+			<li className={`p-4`}>
+				<NavLink 
+					className={'font-bold text-white uppercase'} 
+					to={'admin'}>Inicio</NavLink>
 			</li>
-			<li>
-				<NavLink className={'font-bold'} to={'client'}>Clientes</NavLink>
+			<li className={`p-4`}>
+				<NavLink 
+					className={'font-bold text-white uppercase'} 
+					to={'client'}>Clientes</NavLink>
 			</li>
-			<li>
-				<NavLink className={'font-bold'} to={'plans'}>Planes</NavLink>
+			<li className={`p-4`}>
+				<NavLink 
+					className={'font-bold text-white uppercase'} 
+					to={'plans'}>Planes</NavLink>
 			</li>
-			{/* <li>Planes</li> */}
 		</ul>
 	</nav>
   )
