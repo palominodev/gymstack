9a3a7cd82e4479f83684ddf6ca86df80b8e62576
palominodev <jeremypalomino.dev@gmail.com>
2023-07-10 18:52:55 -0500

    Style Client Table View

--- src/GymStack/components/RowTableUser.jsx
+++ src/GymStack/components/RowTableUser.jsx
@@ -1,13 +1,13 @@
 export const RowTableUser = () => {
   return (
-	<tr>
+	<tr className="even:bg-neutral-700">
 		<td className="border p-3" >1248794</td>
 		<td className="border p-3" >Jeremy Scott</td>
 		<td className="border p-3" >Palomino Fernandez</td>
 		<td className="border p-3" >902966065</td>
 		<td className="border p-3" >jeremy@google.com</td>
 		<td className="border p-3" >Pro</td>
-		<td className="border p-3" >12-03-2023</td>
+		<td className="border p-3" >12/03/2023</td>
 		<td className="border p-3" >Activo</td>
 	</tr>
   )
--- src/GymStack/view/Clients.jsx
+++ src/GymStack/view/Clients.jsx
@@ -4,30 +4,40 @@ import { RowTableUser } from "../components/RowTableUser"
 export const Clients = () => {
   const navigate = useNavigate()
   return (
-    <section className="p-3">
-      <div className="flex justify-between">
-        <h2 className="text-4xl font-bold" >Clientes</h2>
+    <section className="p-3 bg-stone-800 min-h-screen">
+      <div className="flex justify-between mb-4 flex-wrap">
+        <h2 className="text-5xl md:text-6xl font-bold" >Clientes</h2>
         <button 
           onClick={()=> navigate('/client/new')}
-          className="mx-4 p-2 bg-orange-700 font-bold text-white rounded-md transition-all duration-300 hover:bg-orange-600">
+          className="mx-4 p-2 bg-orange-700 font-bold text-white rounded-md transition-all duration-300 hover:bg-orange-600 md:text-lg">
             Crear nuevo
         </button>
+        <input 
+          className="block w-full my-4 text-xl p-2 outline-none rounded-md border-2 border-black" 
+          placeholder="Ingresa nombre..."
+          type="text" 
+          name="search_user" 
+          id="search_user" />
       </div>
       <section className="overflow-x-scroll lg:overflow-hidden">
-        <table>
-          <thead>
-            <tr>
+        <table className="text-white">
+          <thead className="bg-stone-950">
+            <tr >
               <th className="border p-3">ID</th>
               <th className="border p-3">Nombre</th>
               <th className="border p-3">Apellido</th>
               <th className="border p-3">Telefono</th>
               <th className="border p-3">Correo</th>
               <th className="border p-3">Suscripcion</th>
-              <th className="border p-3">Vence el</th>
+              <th className="border p-3">Vence</th>
               <th className="border p-3">Estado</th>
             </tr>
           </thead>
           <tbody>
+            <RowTableUser />
+            <RowTableUser />
+            <RowTableUser />
+            <RowTableUser />
             <RowTableUser />
             <RowTableUser />
             <RowTableUser />
