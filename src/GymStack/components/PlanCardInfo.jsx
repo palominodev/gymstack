73a1963641c05fe081a8e5fd6ce4c2be54ea80d5
palominodev <jeremypalomino.dev@gmail.com>
2023-07-26 14:26:11 -0500

    style: card info

--- src/GymStack/components/PlanCardInfo.jsx
+++ src/GymStack/components/PlanCardInfo.jsx
@@ -3,9 +3,9 @@ import { useSelector } from "react-redux"
 export const PlanCardInfo = () => {
   const { activePlans } = useSelector(state => state.gymStack)
   return (
-	<article className="flex flex-col justify-center items-center bg-sky-300 rounded-md p-2 w-1/2" >
-      <p className="text-2xl font-bold text-center">Planes</p>
-      <span className="text-5xl" >{activePlans}</span>
+	<article className="flex flex-col items-center justify-center bg-sky-300 rounded-md p-2 w-1/2 sm:flex-row" >
+      <p className="w-40 text-2xl font-bold text-center">Planes</p>
+      <span className="text-5xl sm:text-7xl" >{activePlans}</span>
     </article>
   )
 }
