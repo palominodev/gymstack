10e59f0a310354eaac83f9dc970ee971cdd14ec1
palominodev <jeremypalomino.dev@gmail.com>
2023-07-29 17:47:41 -0500

    feat: redireccionar a las vistas cuando le das click a las cards de información

--- src/GymStack/components/PlanCardInfo.jsx
+++ src/GymStack/components/PlanCardInfo.jsx
@@ -1,10 +1,12 @@
 import { MdLibraryBooks } from "react-icons/md"
 import { useSelector } from "react-redux"
+import { useNavigate } from "react-router-dom"
 
 export const PlanCardInfo = () => {
+  const navigate = useNavigate()
   const { activePlans } = useSelector(state => state.gymStack)
   return (
-	<article className="flex flex-col items-center justify-center bg-sky-300 rounded-md p-2 w-1/2 sm:flex-row" >
+	<article onClick={()=> navigate('/plans')} className="flex flex-col items-center justify-center bg-sky-300 rounded-md p-2 w-1/2 sm:flex-row" >
       <MdLibraryBooks className="text-5xl" />
       <p className="w-40 text-2xl font-bold text-center">Planes</p>
       <span className="text-5xl sm:text-7xl" >{activePlans}</span>
--- src/GymStack/components/UserCardInfo.jsx
+++ src/GymStack/components/UserCardInfo.jsx
@@ -1,11 +1,13 @@
 import { FaUsers } from "react-icons/fa"
 import { useSelector } from "react-redux"
+import { useNavigate } from "react-router-dom"
 
 export const UserCardInfo = () => {
+  const navigate = useNavigate()
   const { activeUsers } = useSelector(state => state.gymStack)
 
   return (
-    <article className="flex flex-col items-center justify-center bg-lime-300 rounded-md p-2 w-1/2 sm:flex-row" >
+    <article onClick={() => navigate("/client")} className="flex flex-col items-center justify-center bg-lime-300 rounded-md p-2 w-1/2 sm:flex-row" >
       <FaUsers className="text-5xl" />
       <p className="w-40 text-2xl font-bold text-center">Clientes activos</p>
       <span className="text-5xl sm:text-7xl" >{activeUsers}</span>
