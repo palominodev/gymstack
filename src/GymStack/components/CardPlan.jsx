e5b8da8486e53ae13672a4d9d6c8681aabda8993
palominodev <jeremypalomino.dev@gmail.com>
2023-07-29 17:42:57 -0500

    style: add icons

--- src/GymStack/components/CardPlan.jsx
+++ src/GymStack/components/CardPlan.jsx
@@ -1,29 +1,30 @@
 import { useDispatch } from "react-redux"
 import { startDeletePlan } from "../../store/gymstack/thunks"
+import { FaTrash } from "react-icons/fa"
 
-export const CardPlan = ({price,name='Plan Name',beneficios=[], id}) => {
+export const CardPlan = ({ price, name = 'Plan Name', beneficios = [], id }) => {
 	const dispatch = useDispatch()
 	const onDelete = () => {
 		dispatch(startDeletePlan(id))
 	}
-  return (
-	<article 
-		className="flex flex-col items-center rounded-md p-4 bg-orange-300 justify-between font-sans">
-		<p className="text-3xl text-center mb-2">{name}</p>
-		<p className="text-3xl text-center mb-4 justify-self-start" >
-			<span className="text-7xl font-bold">{price}</span> soles
-		</p>
-		<ul className="w-2/3">
-			{
-				beneficios.map(benef => (
-					<li key={benef} className="p-1" >- {benef}</li>
-				))
-			}
-		</ul>
-		<button 
-			className="bg-red-500 py-2 px-4 font-bold rounded-md text-lg"
-			onClick={onDelete}	
-		>Borrar</button>
-	</article>
-  )
+	return (
+		<article
+			className="flex flex-col items-center rounded-md p-4 bg-orange-300 justify-between font-sans">
+			<p className="text-3xl text-center mb-2">{name}</p>
+			<p className="text-3xl text-center mb-4 justify-self-start" >
+				<span className="text-7xl font-bold">{price}</span> soles
+			</p>
+			<ul className="w-2/3">
+				{
+					beneficios.map(benef => (
+						<li key={benef} className="p-1" >- {benef}</li>
+					))
+				}
+			</ul>
+			<button
+				className="flex items-center gap-1 bg-red-500 py-2 px-4 font-bold rounded-md text-lg"
+				onClick={onDelete}
+			><FaTrash />Borrar</button>
+		</article>
+	)
 }	
--- src/GymStack/components/UserCardInfo.jsx
+++ src/GymStack/components/UserCardInfo.jsx
@@ -1,3 +1,4 @@
+import { FaUsers } from "react-icons/fa"
 import { useSelector } from "react-redux"
 
 export const UserCardInfo = () => {
@@ -5,6 +6,7 @@ export const UserCardInfo = () => {
 
   return (
     <article className="flex flex-col items-center justify-center bg-lime-300 rounded-md p-2 w-1/2 sm:flex-row" >
+      <FaUsers className="text-5xl" />
       <p className="w-40 text-2xl font-bold text-center">Clientes activos</p>
       <span className="text-5xl sm:text-7xl" >{activeUsers}</span>
     </article>
