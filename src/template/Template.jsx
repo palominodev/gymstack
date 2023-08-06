dd5496e2bc3423eb727b7900f64a13e8425a2631
palominodev <jeremypalomino.dev@gmail.com>
2023-08-05 22:43:10 -0500

    style: navbar fijado

--- src/template/Template.jsx
+++ src/template/Template.jsx
@@ -6,11 +6,16 @@ export const Template = ({children}) => {
 	const [slide, setSlide] = useState(false)
   return (
 	<div className="flex relative">
+		<div onClick={() => setSlide(state => !state)}
+			className="sm:invisible peer/slide z-20 fixed left-4 top-4 text-3xl text-white bg-orange-500 rounded-full p-1">
+
 		<MdMenu 
-			onClick={() => setSlide(state => !state)}
-			className="sm:invisible peer/slide z-20 absolute left-5 top-5 text-3xl text-white"/>
+			// onClick={() => setSlide(state => !state)}
+			// className="sm:invisible peer/slide z-20 fixed left-5 top-5 text-3xl text-white"
+		/>
+		</div>
 		{/* <input className="peer/slide z-10 absolute left-20 bg-green-500 " type="checkbox" name="name" id="name" /> */}
-		<header className={`absolute sm:sticky sm:-left-16 h-screen peer-checked/slide:left-0 ${ slide ? 'left-0' : '-left-40' }  bg-orange-500 flex w-40 sm:w-16 sm:hover:w-40 peer group transition-all overflow-hidden z-10`}>
+		<header className={`fixed sm:sticky sm:-left-16 h-screen peer-checked/slide:left-0 ${ slide ? 'left-0' : '-left-40' }  bg-orange-500 flex w-40 sm:w-16 sm:hover:w-40 peer group transition-all overflow-hidden z-10`}>
 			<Navbar slide={slide} setSlide={setSlide} />
 		</header>
 		<main className="pt-10 sm:pt-0  bg-neutral-800 min-h-screen w-full sm:w-[calc(100%_-_4rem)] sm:peer-hover:w-[calc(100%_-_10rem)] transition-all">
