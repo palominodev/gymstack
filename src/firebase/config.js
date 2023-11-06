710224a6aff07f3fe77df1236b544f3782d0d703
palominodev <jeremypalomino.dev@gmail.com>
2023-11-06 18:26:49 -0500

    security: Añadiendo variables de entornos para proteger los datos

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,13 +2,21 @@ import { initializeApp } from "firebase/app"
 import { getFirestore } from 'firebase/firestore'
 import { getAuth } from "firebase/auth"
 
+// export const firebaseConfig = {
+//   apiKey: "AIzaSyDE33C0d_zZhlJtmEtrO1qWBHOxaBgunTg",
+//   authDomain: "gymstack-caeaf.firebaseapp.com",
+//   projectId: "gymstack-caeaf",
+//   storageBucket: "gymstack-caeaf.appspot.com",
+//   messagingSenderId: "864620271061",
+//   appId: "1:864620271061:web:82e9f8ece85f5e838c595c"
+// };
 export const firebaseConfig = {
-  apiKey: "AIzaSyDE33C0d_zZhlJtmEtrO1qWBHOxaBgunTg",
-  authDomain: "gymstack-caeaf.firebaseapp.com",
-  projectId: "gymstack-caeaf",
-  storageBucket: "gymstack-caeaf.appspot.com",
-  messagingSenderId: "864620271061",
-  appId: "1:864620271061:web:82e9f8ece85f5e838c595c"
+  apiKey: process.env.API_KEY_SECRET,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGE_SENDER_ID,
+  appId: process.env.APP_ID
 };
 
 export const FirebaseApp = initializeApp(firebaseConfig)
